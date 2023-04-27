89f805ff80c2f4e45a69b9f5e96ea754c49f8758
Bojan Potočnik <bpotocnik@plume.com>
2023-04-27 18:19:31 +0200

    sw: build.py: Detect required local CircuitPython modules
    Detect which CircuitPython libraries (modules from lib/ directory) are
    used by the main script.

--- SW/build.py
+++ SW/build.py
@@ -6,7 +6,10 @@ import tempfile
 import urllib.request
 import zipfile
 from io import BytesIO
+from modulefinder import ModuleFinder
 from pathlib import Path
+from types import ModuleType
+from typing import List
 
 lib_dir = Path("lib")
 src_dir = Path("src")
@@ -43,5 +46,33 @@ def check_adafruit_libraries() -> None:
         download_adafruit_bundle(dp)
 
 
+def check_module_requirements(script_name: str = "code.py") -> List[ModuleType]:
+    script = src_dir.joinpath(script_name).resolve()
+
+    # The target script will be run on the target device using CircuitPython runtime,
+    # not the runtime and environment that this current script is running in.
+    # Therefore, do not use default sys.path for importing the modules!
+    finder = ModuleFinder(path=[])
+    # Run the script once to get the list of all modules that were imported or attempted to be imported
+    finder.run_script(script.as_posix())
+    # Successfully imported modules are built-in modules (empty path)
+    modules = list(finder.modules.keys())
+    print(f"{script} uses built-in modules: {', '.join(m for m in modules if m != '__main__')}")
+    # Modules failed to import are most likely CircuitPython libraries
+
+    # Rerun the script with CircuitPython library paths added
+    finder = ModuleFinder(path=[script.parent.as_posix(), lib_dir.resolve().as_posix()] +
+                               [dp.as_posix() for dp in lib_dir.resolve().glob("*/lib")])
+    finder.run_script(script.as_posix())
+    # All modules which failed to import initially, but are successfully imported now,
+    # are CircuitPython libraries located in the provided library paths.
+    required_modules = {name: module for name, module in finder.modules.items() if name not in modules}
+
+    print(f"{script} uses CircuitPython libraries: {', '.join(required_modules)}")
+
+    return list(required_modules.values())
+
+
 check_dirs()
 check_adafruit_libraries()
+check_module_requirements()
--- SW/src/code.py
+++ SW/src/code.py
@@ -1,7 +1,30 @@
 import sys
+import time
 
 import board
+import microcontroller
+
+import neopixel
 
 # CircuitPython throws SyntaxError for multiline f-strings - either use single line or concatenation
 print(f"\nRunning on {board.board_id} ({sys.platform}), {sys.implementation.name} " +
       f"{sys.version}/{'.'.join(map(str, sys.implementation.version))}, mpy {sys.implementation.mpy}")
+
+
+def get_gpio(of_pin: microcontroller.Pin) -> str:
+    # noinspections PyUnresolvedReferences
+    pins = microcontroller.pin
+    return next(p for p in dir(pins) if getattr(pins, p) == of_pin)
+
+
+if hasattr(board, "NEOPIXEL"):
+    print(f"Testing NeoPixel on {get_gpio(board.NEOPIXEL)}")
+    pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.5)
+
+    pixel.fill(0xFF0000)
+    time.sleep(0.5)
+    pixel.fill(0x00FF00)
+    time.sleep(0.5)
+    pixel.fill(0x0000FF)
+    time.sleep(0.5)
+    pixel.fill(0x000000)
